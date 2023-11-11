13e4a1b1260c161d6fb6a7d18f33a7ae8fccfd94
DanielLRomano <romanodaniel1012@gmail.com>
2023-11-10 21:38:27 -0300

    10/11

---
+++ PWFE2Semestre/ProjetoVue/projeto-clima-api/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <Clima />
+  </div>
+</template>
+<script>
+import Clima from './components/clima.vue';
+export default {
+  name: 'App',
+
+  components: {
+    Clima,
+  },
+};
+</script>
